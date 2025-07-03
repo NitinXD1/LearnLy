71c957500d0902bafbd0a4029464b68dbd3a9740
NitinXD1 <nitinkachera98@gmail.com>
2025-07-03 20:38:28 +0530

    feat: integrate React Router and React Query, add new pages and update dependencies
    - Added routing with React Router for Home, Sign Up, Log In, Chat, Call, Onboarding, and Notifications pages.
    - Integrated React Query for data fetching with a new axios instance.
    - Updated package.json to include new dependencies: @tanstack/react-query, axios, react-hot-toast, and daisyui.
    - Created PostCSS configuration for Tailwind CSS and removed App.css.
    - Added Tailwind CSS configuration and imported Tailwind directives in index.css.
    - Removed unused assets and styles to streamline the project structure.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Route,Routes } from "react-router"
+import HomePage from './pages/HomePage.jsx'
+import SignUpPage from "./pages/SignUpPage.jsx"
+import LogInPage from "./pages/LogInPage.jsx"
+import ChatPage from "./pages/ChatPage.jsx"
+import NotificationPage from "./pages/NotificationsPage.jsx"
+import OnBoardingPage from "./pages/OnBoardingPage.jsx"
+import CallPage from "./pages/CallPage.jsx"
+
+import {Toaster} from "react-hot-toast"
+import { useQuery } from "@tanstack/react-query"
+import { axiosInstance } from "./lib/axios.js"
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const {data} = useQuery({
+    queryKey: ["todos"],
+    queryFn: async () => {
+      const response = await axiosInstance.get('/auth/user')
+      return response.data
+    }
+  })
+
+  console.log(data)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="h-screen" data-theme="night">
+        <Routes>
+          <Route path="/" element={<HomePage />}/>
+          <Route path="/signup" element={<SignUpPage />}/>
+          <Route path="/login" element={<LogInPage />}/>
+          <Route path="/chat" element={<ChatPage />}/>
+          <Route path="/call" element={<CallPage />}/>
+          <Route path="/onboard" element={<OnBoardingPage />}/>
+          <Route path="/notifications" element={<NotificationPage />}/>
+        </Routes>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <Toaster />
     </>
   )
 }
